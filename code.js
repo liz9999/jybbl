dc1a32d403e9227433d9014eb3cfa6e8bb1f513e
liz9999 <69991641+liz9999@users.noreply.github.com>
2022-09-29 19:32:43 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 var bd = document.getElementsByClassName('border')
 var ts = document.getElementsByClassName('translation')
+var test = document.getElementById('test')
 
 function translate() {
     for (i = 0; i < bd.length; i++) {
@@ -13,3 +14,28 @@ function translate() {
     }
     }
    }
+
+test.addEventListener('click', function () {
+    for (i = 0; i < bd.length; i++) {
+      if (bd[i].style.display !== 'block') {
+            bd[i].style.display = 'block';
+            ts[i].style.display = 'block';
+    }
+        else {
+            bd[i].style.display = 'none';
+            ts[i].style.display = 'none';
+    }
+    }
+});
+test.addEventListener('touchstart', function () {
+    for (i = 0; i < bd.length; i++) {
+      if (bd[i].style.display !== 'block') {
+            bd[i].style.display = 'block';
+            ts[i].style.display = 'block';
+    }
+        else {
+            bd[i].style.display = 'none';
+            ts[i].style.display = 'none';
+    }
+    }
+});
