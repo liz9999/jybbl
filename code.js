f2aa1602bfd0dbb5fdfaffb9ca4663db8b87d205
liz9999 <69991641+liz9999@users.noreply.github.com>
2022-09-29 19:38:38 -0400

    Update code.js

--- code.js
+++ code.js
@@ -14,28 +14,3 @@ function translate() {
     }
     }
    }
-
-test.addEventListener('click', function () {
-    for (i = 0; i < bd.length; i++) {
-      if (bd[i].style.display !== 'block') {
-            bd[i].style.display = 'block';
-            ts[i].style.display = 'block';
-    }
-        else {
-            bd[i].style.display = 'none';
-            ts[i].style.display = 'none';
-    }
-    }
-});
-test.addEventListener('touchstart', function () {
-    for (i = 0; i < bd.length; i++) {
-      if (bd[i].style.display !== 'block') {
-            bd[i].style.display = 'block';
-            ts[i].style.display = 'block';
-    }
-        else {
-            bd[i].style.display = 'none';
-            ts[i].style.display = 'none';
-    }
-    }
-});
