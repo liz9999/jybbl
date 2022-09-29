dd11ffbd95ab1b13d12def6524667ad4d67ae7be
liz9999 <69991641+liz9999@users.noreply.github.com>
2022-09-29 17:52:34 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
 var test = document.getElementById('test');
+var bd = document.getElementById('border');
+var ts = document.getElementById('translation')
 
-test.onclick = function() {
-    var bd = document.getElementById('border');
-    var ts = document.getElementById('translation')
+function translate() {
     if (bd.style.display !== 'block') {
         bd.style.display = 'block';
 }
     else {
-        ts.style.display = 'none';
+        bd.style.display = 'none';
 }
     if (ts.style.display !== 'block') {
         ts.style.display = 'block';
