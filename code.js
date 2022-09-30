fb73fa2dfa1c4a69f71982d4613a5bf4b5dd04f6
liz9999 <69991641+liz9999@users.noreply.github.com>
2022-09-29 21:26:30 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 var bd = document.getElementsByClassName('border')
 var ts = document.getElementsByClassName('translation')
-var test = document.getElementById('test')
+var te = document.getElementById('te')
 
 function translate() {
     for (i = 0; i < bd.length; i++) {
@@ -14,3 +14,16 @@ function translate() {
     }
     }
    }
+
+te.addEventListener("touchstart", function() {
+    for (i = 0; i < bd.length; i++) {
+      if (bd[i].style.display !== 'block') {
+            bd[i].style.display = 'block';
+            ts[i].style.display = 'block';
+    }
+        else {
+            bd[i].style.display = 'none';
+            ts[i].style.display = 'none';
+    }
+    }
+});
